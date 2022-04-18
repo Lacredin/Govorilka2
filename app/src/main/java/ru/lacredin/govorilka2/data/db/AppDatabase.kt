46b5e6894ba60d282458994e181b84834efdbba2
STRATEG <lacredin@gmail.com>
2022-04-18 18:35:17 +0300

    fix error room

--- app/src/main/java/ru/lacredin/govorilka2/data/db/AppDatabase.kt
+++ app/src/main/java/ru/lacredin/govorilka2/data/db/AppDatabase.kt
@@ -1,6 +1,5 @@
 package ru.lacredin.govorilka2.data.db
 
-import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import ru.lacredin.govorilka2.data.db.dao.UserDao
@@ -11,9 +10,9 @@ import ru.lacredin.govorilka2.data.db.entity.User
     entities = [
         User::class
     ],
-    autoMigrations = [
-        AutoMigration(from = 1, to = 2)
-    ]
+//    autoMigrations = [
+//        AutoMigration(from = 1, to = 2)
+//    ]
 )
 abstract class AppDatabase : RoomDatabase() {
     companion object{
